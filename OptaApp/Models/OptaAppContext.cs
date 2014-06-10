238b55a31164c62707c89e54f57dd11b3e7cf2b1
rdiazgow <djjosefs@yahoo.com>
2014-06-10 10:16:26 -0500

    Update OptaAppContext.cs

--- OptaApp/Models/OptaAppContext.cs
+++ OptaApp/Models/OptaAppContext.cs
@@ -12,9 +12,19 @@ namespace OptaApp.Models
         // Nota: esta operación destruirá y volverá a crear la base de datos con cada cambio de modelo.
         // 
         // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<OptaApp.Models.OptaAppContext>());
+        
+        private string _schemaName = string.Emty;
 
-        public OptaAppContext() : base("name=OptaAppContext")
+        public OptaAppContext(string connectionName, string schemaName) : base(connectionName)
         {
+            _schemaName =schemaName;
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializaer<OptaAppContext> (new CreateDatabaseIfNotExists<OptaAppContext>());
+            modelBuilder.Entity<Persona>().ToTable("Persona", _schemaName);
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Persona> Personas { get; set; }
